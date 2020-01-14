7d31f5067a59830ec4c6d1d2aa35fb60a5a0461b
Fma965 <fma965@cyanlabs.net>
2020-01-14 20:28:32 +0000

    Added support for AX

--- src/corsairpsu/usr/local/emhttp/plugins/corsairpsu/status.php
+++ src/corsairpsu/usr/local/emhttp/plugins/corsairpsu/status.php
@@ -1,47 +1,127 @@
 <?php
-$stdout = shell_exec('/usr/local/bin/corsairmi 2>&1');
+$settings = parse_ini_file( "/boot/config/plugins/corsairpsu/corsairpsu.cfg" );
+
+if ($settings["TYPE"] == "corsairmi") {
+	$stdout = shell_exec('/usr/local/bin/corsairmi 2>&1');
+} elseif ($settings["TYPE"] == "cpsumoncli") {
+	$stdout = shell_exec('/usr/local/bin/cpsumon/cpsumoncli ' . $settings["TTY"] . ' 2>&1');
+	//Debug Testing - $stdout = file_get_contents("/usr/local/emhttp/plugins/corsairpsu/text.txt");
+} else {
+	die("There is an error with your configuration!");
+}
 $re     = '/(?<key>[^:]+):\s+\'*(?<value>[^\n\']+)\'*\s*/';
 preg_match_all($re, $stdout, $matches, PREG_SET_ORDER, 0);
 foreach ($matches as $match)
     $data[$match['key']] = $match['value'];
 if (!isset($data))
     exit(json_encode(array($stdout)));
-$capacity = filter_var($data['product'], FILTER_SANITIZE_NUMBER_INT);
-$load     = round($data['total watts'] / $capacity * 100);
-$output0_load     = round($data['output0 watts'] / $capacity * 100);
-$output1_load     = round($data['output1 watts'] / $capacity * 100);
-$output2_load     = round($data['output2 watts'] / $capacity * 100);
-// Sets keys for the 3 outputs to 12v, 5v and 3v
-$output0 = substr($data['output0 volts'], 0, strrpos($data['output0 volts'], '.'));
-$output1 = substr($data['output1 volts'], 0, strrpos($data['output1 volts'], '.'));
-$output2 = substr($data['output2 volts'], 0, strrpos($data['output2 volts'], '.'));
 
-// Removes raw timestamp on uptime / total hours leaving just the Xd, Xh text
-preg_match('/\((.*?)\)/', $data['uptime'], $uptime);
-preg_match('/\((.*?)\)/', $data['powered'], $poweredon);
+if ($settings["TYPE"] == "corsairmi") {
+	$capacity = filter_var($data['product'], FILTER_SANITIZE_NUMBER_INT);
+	$load     = round($data['total watts'] / $capacity * 100);
+	
+	$output0_load     = round($data['output0 watts'] / $capacity * 100);
+	$output1_load     = round($data['output1 watts'] / $capacity * 100);
+	$output2_load     = round($data['output2 watts'] / $capacity * 100);
+	
+	// Sets keys for the 3 outputs to 12v, 5v and 3v
+	$output0 = substr($data['output0 volts'], 0, strrpos($data['output0 volts'], '.'));
+	$output1 = substr($data['output1 volts'], 0, strrpos($data['output1 volts'], '.'));
+	$output2 = substr($data['output2 volts'], 0, strrpos($data['output2 volts'], '.'));
 
-// Adds the keys and values to an array, named appropiately
-$json = array(
-    'temp1' => $data['temp1'],
-    'temp2' => $data['temp2'],
-    'fan_rpm' => $data['fan rpm'],
-    'capacity' => $capacity,
-    "{$output0}v_watts" => $data['output0 watts'],
-    "{$output1}v_watts" => $data['output1 watts'],
-    "{$output2}v_watts" => $data['output2 watts'],
-    'watts' => $data['total watts'],
-    'load' => $load,
-	"{$output0}v_load" => $output0_load,
-	"{$output1}v_load" => $output1_load,
-	"{$output2}v_load" => $output2_load,
-    'vendor' => $data['vendor'],
-    'product' => $data['product'],
-    'uptime' => $uptime[1],
-    'uptime_raw' => substr($data['uptime'], 0, strpos($data['uptime'], ' ')),
-    'poweredon' => $poweredon[1],
-    'poweredon_raw' => substr($data['powered'], 0, strpos($data['powered'], ' '))
-);
+	// Removes raw timestamp on uptime / total hours leaving just the Xd, Xh text
+	preg_match('/\((.*?)\)/', $data['uptime'], $uptime);
+	preg_match('/\((.*?)\)/', $data['powered'], $poweredon);
+	
+	$efficiency = 
 
+	// Adds the keys and values to an array, named appropiately
+	$json = array(
+		'temp1' => $data['temp1'],
+		'temp2' => $data['temp2'],
+		'fan_rpm' => $data['fan rpm'],
+		'capacity' => $capacity,
+		"{$output0}v_watts" => $data['output0 watts'],
+		"{$output1}v_watts" => $data['output1 watts'],
+		"{$output2}v_watts" => $data['output2 watts'],
+		'watts' => $data['total watts'],
+		'load' => $load,
+		"{$output0}v_load" => $output0_load,
+		"{$output1}v_load" => $output1_load,
+		"{$output2}v_load" => $output2_load,
+		'vendor' => $data['vendor'],
+		'product' => $data['product'],
+		'uptime' => $uptime[1],
+		'uptime_raw' => substr($data['uptime'], 0, strpos($data['uptime'], ' ')),
+		'poweredon' => $poweredon[1],
+		'poweredon_raw' => substr($data['powered'], 0, strpos($data['powered'], ' ')),
+		'efficiency' => 0
+	);	
+} else {
+	
+	$capacity = filter_var($data["PSU type"], FILTER_SANITIZE_NUMBER_INT);
+	$input = floatval($data['Input power']);
+	$load = round($input / $capacity * 100);
+
+
+	$rail_5v = explode(",", $data['5V Rail']);
+	$rail_3v = explode(",", $data['3.3V Rail']);
+
+	$rail_5v_watts     = round(floatval($rail_5v[2]),2);
+	$rail_3v_watts     = round(floatval($rail_3v[2]),2);
+	$rail_12v = $input - $rail_5v_watts - $rail_3v_watts;
+	
+	$rail_12v_watts     = round(floatval($rail_12v),2);
+
+	$rail_5v_load     = round(floatval($rail_5v[2]) / $capacity * 100);
+	$rail_3v_load     = round(floatval($rail_3v[2]) / $capacity * 100);
+	$rail_12v_load     = round(floatval($rail_12v) / $capacity * 100);
+
+	$json = array(
+		'temp1' => floatval($data["Temperature"]),
+		'temp2' => floatval($data["Temperature"]),
+		'fan_rpm' => floatval($data["Fan speed"]),
+		'capacity' => $capacity,
+		"12v_watts" => $rail_12v_watts,
+		"5v_watts" => $rail_5v_watts,
+		"3v_watts" => $rail_3v_watts,
+		'watts' => $input,
+		'load' => $load,
+		"12v_load" => $rail_12v_load,
+		"5v_load" => $rail_5v_load,
+		"3v_load" => $rail_3v_load,
+		'vendor' => "CORSAIR",
+		'product' => str_replace("\r","",$data["PSU type"]),
+		'uptime' => 0,
+		'uptime_raw' => 0,
+		'poweredon' => 0,
+		'poweredon_raw' => 0,
+		'efficiency' => floatval($data['Efficiency'])
+	);
+}
 header('Content-Type: application/json');
 echo json_encode($json);
+
+/*
+$data["Dongle version"] => 0.9
+$data["PSU type"] => AX860i
+$data["Fan mode"] => Auto
+$data["Fan speed"] => 0.00 RPM
+$data["Temperature"] => 39.75 °C
+$data["Voltage"] => 222.00 V
+$data["Current"] => 0.62 A
+$data["Input power"] => 133.38 W
+$data["Output power"] => 118.73 W
+$data["Efficiency"] => 89.02 %
+$data["PCIe 00 Rail"] => 12.00 V, 0.00 A, 0.00 W, OCP disabled (Limit: 40.00 A)
+$data["PCIe 01 Rail"] => 12.02 V, 0.00 A, 0.00 W, OCP enabled  (Limit: 29.94 A)
+$data["PCIe 02 Rail"] => 12.02 V, 0.00 A, 0.00 W, OCP enabled  (Limit: 30.00 A)
+$data["PCIe 03 Rail"] => 12.00 V, 0.00 A, 0.00 W, OCP enabled  (Limit: 29.94 A)
+$data["PCIe 04 Rail"] => 12.02 V, 0.00 A, 0.00 W, OCP enabled  (Limit: 29.94 A)
+$data["PCIe 05 Rail"] => 12.02 V, 0.00 A, 0.00 W, OCP enabled  (Limit: 30.00 A)
+$data["ATX Rail"] => 12.00 V, 0.00 A, 0.00 W, OCP disabled (Limit: 40.00 A)
+$data["Peripheral Rail"] => 12.00 V, 3.00 A, 37.50 W, OCP enabled  (Limit: 29.94 A)
+$data["5V Rail"] => 5.02 V, 3.75 A, 18.65 W
+$data["3.3V Rail"] => 3.31 V, 3.81 A, 12.56 W
+*/
 ?>
